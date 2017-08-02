4fedef2074b13be8aa8ea45caa065fbd2cbc314d
Aluno <lucascoliado@hotmail.com>
2017-08-02 15:31:26 -0300

    Adicionado script para criar container e executar seu bash

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 sudo docker build -t mysql-test .
-sudo docker run -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
+if [ $# -eq 0 ]
+  then
+    echo "Sem argumentos"
+    sudo docker run --name banco -d -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
+    ./dbash.sh banco
+  else
+    sudo docker run --name $1 -d -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
+    ./dbash.sh $1
+fi
+
+
 
