fef19303fb958b8f838d20c5e343333eafb96fcc
Aluno <lucascoliado@hotmail.com>
2017-08-02 14:12:25 -0300

    Modificado caminho volume

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo docker build -t mysql-test .
-sudo docker run -v "$(pwd)"/MySQL_Files:/var/container_data/mysql mysql-test
+sudo docker run -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
 
