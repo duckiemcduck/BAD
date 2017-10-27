a5e77ffc4a5b5d30a291614d0ff37fc3427a37f7
Aluno <lucascoliado@hotmail.com>
2017-10-27 15:57:31 -0200

    Adicionar arquivos projeto

--- run-docker.sh
+++ run-docker.sh
@@ -3,10 +3,10 @@ sudo docker build -t mysql-test .
 if [ $# -eq 0 ]
   then
     echo "Sem argumentos"
-    sudo docker run --name banco -d -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
+    sudo docker run --name banco -d --publish-all=true -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
     ./dbash.sh banco
   else
-    sudo docker run --name $1 -d -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test
+    sudo docker run --name $1 -d --publish-all=true -v "$(pwd)"/MySQL_Files:/var/lib/mysql mysql-test 
     ./dbash.sh $1
 fi
 
