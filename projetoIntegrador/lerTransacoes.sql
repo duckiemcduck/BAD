8c5e41fd8a20b373d21da361be7008c47eb73dc3
Aluno <lucascoliado@hotmail.com>
2017-11-24 15:30:31 -0200

    novo sql bacana

--- projetoIntegrador/lerTransacoes.sql
+++ projetoIntegrador/lerTransacoes.sql
@@ -5,5 +5,15 @@ WITH transacoes as
     FROM transacao INNER JOIN almoxarifado
     ON almoxarifado.idalmoxarifado = transacao.idalmoxarifado
 )
-SELECT transacoes.almoxarifado as almoxarifado, usuario.nome, transacoes.data_solicitada, transacoes.quantidade_entregue, transacoes.devolvida, transacoes.data_devolvida
-FROM usuario INNER JOIN transacoes ON usuario.idusuario = transacoes.idusuario AND usuario.idalmoxarifado = transacoes.idalmoxarifado AND transacoes.entregue=1;
+, itemalmox as 
+(
+	SELECT item.idalmoxarifado, item.info as nome, item.iditem as iditem FROM item INNER JOIN transacoes
+    ON item.idalmoxarifado = transacoes.idalmoxarifado AND item.iditem = transacoes.iditem
+)
+, transacaodepurada as
+(
+	SELECT transacoes.idalmoxarifado,transacoes.iditem as transacaoiditem, transacoes.almoxarifado as almoxarifado, usuario.nome, transacoes.data_solicitada, transacoes.quantidade_entregue, transacoes.devolvida, transacoes.data_devolvida
+	FROM usuario INNER JOIN transacoes ON usuario.idusuario = transacoes.idusuario AND usuario.idalmoxarifado = transacoes.idalmoxarifado AND transacoes.entregue=1
+)
+SELECT itemalmox.nome, transacaodepurada.almoxarifado as almoxarifado, transacaodepurada.nome as autor, transacaodepurada.data_solicitada, transacaodepurada.quantidade_entregue, transacaodepurada.devolvida, transacaodepurada.data_devolvida FROM transacaodepurada INNER JOIN itemalmox ON itemalmox.iditem = transacaodepurada.transacaoiditem AND itemalmox.idalmoxarifado = transacaodepurada.idalmoxarifado;
+
